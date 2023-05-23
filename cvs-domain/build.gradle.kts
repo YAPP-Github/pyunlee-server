48d9c308288ac5fec77821bc24d8635a9764cce7
노경민 <kls1998@naver.com>
2023-05-23 11:40:47 +0900

    feat : 멀티모듈 프로젝트 세팅 (#4)
    * feat : sonarcloud, jacoco 분석 툴 설정
    
    fix : env 파일 기본값 설정
    
    fix : gradle 설정 변경
    
    config : gradle 파일 설정 분리
    
    config : gradle 파일 설정 분리
    
    fix : 설정 분리 재작성
    
    fix : gradlew 권한 부여
    
    config : sonar test
    
    config : jacoco test
    
    config : sonar token
    
    config : disable ci
    
    * ci : disable ci

--- cvs-domain/build.gradle.kts
+++ cvs-domain/build.gradle.kts
@@ -1,5 +1,18 @@
-plugins{
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("mysql:mysql-connector-java:8.0.27")
+    api("com.h2database:h2")
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
 }
 
 allOpen {
@@ -12,8 +25,4 @@ noArg {
     annotation("javax.persistence.Entity") // @Entity가 붙은 클래스에 한해서만 no arg 플러그인을 적용
     annotation("javax.persistence.Embeddable")
     annotation("javax.persistence.MappedSuperclass")
-}
-
-dependencies{
-
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,2 @@
 rootProject.name = "cvs"
-include("cvs-api")
-include("cvs-domain")
-include("cvs-batch")
+include("cvs-api", "cvs-batch", "cvs-domain")
