833f4876f587106c315dcc06f4c084071847e4ff
gengminy <kls1998@naver.com>
2023-05-28 17:57:27 +0900

    chore : 더미 인터페이스 삭제

--- cvs-batch/src/main/kotlin/com/yapp/cvs/job/crawl/CollectorInstruction.kt
+++
@@ -1,9 +0,0 @@
-package com.yapp.cvs.job.crawl
-
-import com.yapp.cvs.domains.product.entity.ProductCategory
-
-interface CollectorInstruction {
-    fun collect(category: ProductCategory): List<ProductCollectorDto>
-
-    fun expandAllProductPage()
-}
