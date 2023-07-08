68caf818429e75b658295f60af415338a28aa7ef
gengminy <kls1998@naver.com>
2023-07-08 17:07:57 +0900

    deploy : test 삭제

--- cvs-admin/src/test/kotlin/com/yapp/cvs/CvsAdminApplicationTests.kt
+++ cvs-admin/src/test/kotlin/com/yapp/cvs/CvsAdminApplicationTests.kt
@@ -1,13 +1,6 @@
 package com.yapp.cvs
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class CvsAdminApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+class CvsAdminApplicationTests
