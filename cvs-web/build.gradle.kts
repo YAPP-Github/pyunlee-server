2d68a1b4a9546aa615f10dda4cad3db289393eca
kingjakeu <jakehsyoo@gmail.com>
2023-07-15 16:00:48 +0900

    feat: web fix

--- cvs-web/build.gradle.kts
+++ cvs-web/build.gradle.kts
@@ -1,21 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
-}
-
-group = "com.yapp"
-version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-    mavenCentral()
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
 
 dependencies {
@@ -24,15 +8,4 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
--- cvs-web/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "cvs-web"
